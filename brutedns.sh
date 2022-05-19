844860c9fc381debac0e36eff45607328a851273
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-05-19 19:14:51 -0300

    Update brutedns.sh

--- brutedns.sh
+++ brutedns.sh
@@ -1,10 +1,17 @@
+#!/usr/bin/env bash
+
 
 #Script que faz bruteforce de subdominios
 
 domain=$1
 
+COR_VERMELHO="\e[31;1;4m \n"
+
+COR_YELLOW="\e[33;1m"
+#-------------------------------------------------------------------------------
+
 while read sub;do
   if host $sub.$domain &> /dev/null;then
-    echo "$sub.$domain";
+    echo -e ${COR_VERMELHO} "Dominios encontrados:" ${COR_YELLOW}"$sub.$domain";
   fi
 done < listadns.txt
